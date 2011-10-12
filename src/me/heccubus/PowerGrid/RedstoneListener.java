c4eaa4ddffc5fbb71e5090188bfc02e1abd889b3
Craig Coffey <craig@coffeyhouse.com>
2011-10-12 08:55:25 -0400

    Documented a bit better...

--- src/me/heccubus/PowerGrid/RedstoneListener.java
+++ src/me/heccubus/PowerGrid/RedstoneListener.java
@@ -21,9 +21,25 @@ public class RedstoneListener extends BlockListener {
 		int bTargetID = bTarget.getTypeId();
 		Block uTarget = bTarget.getRelative(BlockFace.DOWN);
 		int uTargetID = uTarget.getTypeId();
+		Block bTemp;
+		int bTempID = 0;
+		BlockFace bFaces[] = BlockFace.values();
+		BlockFace bFace = null;
 
+		// If the block UNDER the powered block is of the correct type...
 		if (uTargetID == 22) {
-			toggleTorch(bTarget, bTargetID); 
+			// ...check all directions (NORTH, SOUTH, EAST and WEST)...
+			for (int i=0; i<=3; i++) {
+				bFace = bFaces[i];
+				bTemp = uTarget.getRelative(bFace);
+				bTempID = bTemp.getTypeId();
+
+				// ...for any blocks in those directions of the correct type, 
+				// call the toggleTorch method.
+				if (bTempID == 22) {
+					toggleTorch(bTemp, bTargetID);
+				}
+			}
 		}
 	}
 
@@ -31,26 +47,36 @@ public class RedstoneListener extends BlockListener {
 		Block bTarget = tb;
 		Block bTemp;
 		int bTempID = 0;
-		Block uTemp = null;
-		int uTempID = 0;
 		BlockFace bFaces[] = BlockFace.values();
 		BlockFace bFace = null;
 
-		int bID = tID;
-		if (bID == 75) { bID = 0; }
+		// If a dead redstone torch ID is passed, use air instead
+		int bID = (tID == 75) ? 0 : tID;
 
-		bTarget.setTypeId(bID);
-
+		// Get the block above the target block
+		bTemp = bTarget.getRelative(BlockFace.UP);
+		bTempID = bTemp.getTypeId();
+		
+		// Set it to the passed-in block type (air or a powered redstone torch) as long as
+		// it is not ALREADY the passed-in type, and it IS the OTHER type.  Ignore all other
+		// block and material types.
+		if (bTempID != bID && (bTempID == 0 || bTempID == 76)) {
+			bTemp.setTypeId(bID);
+		}
+		
+		// Call this method recursively for all qualifying blocks all around the target block
+		// NORTH, SOUTH, EAST and WEST only.
 		for (int i=0; i<=3; i++) {
 			bFace = bFaces[i];
 			bTemp = bTarget.getRelative(bFace);
 			bTempID = bTemp.getTypeId();
-			uTemp = bTemp.getRelative(BlockFace.DOWN);
-			uTempID = uTemp.getTypeId();
-				
-			if (bTempID != bID && uTempID == 22) {
-				toggleTorch(bTemp, bID);
+			
+			// Only call recursively if the block in that direction is the appropriate type
+			// ...and the block ABOVE that block has not already been set to the target type
+			// (air or a powered redstone torch)
+			if (bTempID == 22 && bTemp.getRelative(BlockFace.UP).getTypeId() != bID) {
+				toggleTorch(bTemp, bID); 
 			}
-		}		
+		}
 	}
 }
